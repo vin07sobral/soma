87cd84c91298145a93f6bd9aaf4c7614173a32b1
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-09-26 16:28:23 -0300

    Add files via upload

---
+++ soma/soma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace revisao_logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            soma = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 != 0)
+                { // testa se i é impar
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
